e7a89c750a2d7223f5f7df58ad4000a7a2d4775b
kemkai <kemkaikub@gmail.com>
2024-11-06 21:55:39 +0700

    add images at next.config.mjsbackend

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        images: {
-            domains: ['images.pexels.com', 'png.pngtree.com', 'localhost', 'backend-pc-production-e77d.up.railway.app'],
-        },
+        domains: ['images.pexels.com', 'png.pngtree.com', 'localhost', 'backend-pc-production-e77d.up.railway.app'],
         remotePatterns: [
             {
-                protocol : 'https',
-                hostname : 'images.pexels.com',
+                protocol: 'https',
+                hostname: 'images.pexels.com',
             },
             {
-                protocol : 'https',
-                hostname : 'png.pngtree.com',
+                protocol: 'https',
+                hostname: 'png.pngtree.com',
             },
             {
                 protocol: 'http',
